1664006bcbc930221ca0406f00a8b115fe9e6f2c
Daniel Gotardo <profe.gotardo@gmail.com>
2021-06-06 02:49:25 -0300

    release/service

--- src/app/page/main-page/main-page.component.ts
+++ src/app/page/main-page/main-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Plano } from 'src/app/model/plano';
+import { Plataforma } from 'src/app/model/plataforma';
+import { PlanoService } from 'src/app/service/plano.service';
+import { PlataformaService } from 'src/app/service/plataforma.service';
 
 @Component({
   selector: 'app-main-page',
@@ -7,9 +11,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
 
-  constructor() { }
+
+  plataformas!: Plataforma[];
+  planos!: Plano[];
+  planosCPT: any;
+  planosTBT: any;
+  planosWF: any;
+
+  constructor(private plataformaService: PlataformaService,
+    private planosList: PlanoService) { }
 
   ngOnInit(): void {
+    this.getPlataformas;
+  }
+
+  getPlataformas() {
+    this.plataformaService.getPlataformas().subscribe(
+      data => {
+        this.plataformas = data;
+
+      },
+      error => {
+        console.log(error)
+      });
+  }
+
+  getPlanosCPT() {
+    this.planosList.getPlanoCPT().subscribe(
+      data => {
+        this.planosCPT = data;
+
+      },
+      error => {
+        console.log(error)
+      });
+  }
+  getPlanosTBT() {
+    this.planosList.getPlanoTBT().subscribe(
+      data => {
+        this.planosTBT = data;
+
+      },
+      error => {
+        console.log(error)
+      });
+  }
+  getPlanosWF() {
+    this.planosList.getPlanoWF().subscribe(
+      data => {
+        this.planosWF = data;
+
+      },
+      error => {
+        console.log(error)
+      });
   }
 
 }
